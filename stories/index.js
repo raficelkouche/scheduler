a565e2d28450f3a6cd20af44cce78a6ae70018f6
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-20 14:12:17 -0500

    add a Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 const days = [
   {
@@ -131,3 +132,9 @@ storiesOf("Appointment", module)
   .add("Appointment with time", () => <Appointment time="12pm"/>)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show 
+    student="Lydia Miller-Jones" 
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />)
