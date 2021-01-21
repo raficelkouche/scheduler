0d0fdd5983c095d99c454bd90d16ee9bc820c020
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-20 20:35:01 -0500

    add list of appointments to the application

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,46 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import { DayList } from "components/DayList"
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Sarah Adams",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm"
+  },
+];
 
 const days = [
   {
@@ -47,7 +87,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment}/>
+        })}
+        <Appointment key="last" time="5pm"/>
       </section>
     </main>
   );
