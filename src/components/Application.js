f5b3fc3837a8de7714a1e592797a711ff6a3b105
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-21 12:54:48 -0500

    combine the states for day and days

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,51 +4,18 @@ import axios from 'axios';
 import "components/Application.scss";
 import { DayList } from "components/DayList"
 import Appointment from "components/Appointment";
+import {getAppointmentsForDay} from "../helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm"
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm"
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Sarah Adams",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm"
-  },
-];
 
 export default function Application(props) {
-  //this has to be removed
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
   
+  const [state, setState] = useState({ day: "Monday", days: [], appointments: {} });
+  const setDay = day => setState({ ...state,day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
+
+  //remove this line
+  const dailyAppointments = [];
+
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
       .then(res => {
@@ -68,8 +35,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+export function getAppointmentsForDay(state, day) {
+  const dayObject = state.days.find((elm) => {
+    return elm.name === day;
+  });
+
+  if(dayObject) {
+    const appointmentsForDay = dayObject.appointments.map(appointment => {
+      return state.appointments[appointment]
+    });
+
+    return appointmentsForDay;
+  }
+  
+  return [];
+}
